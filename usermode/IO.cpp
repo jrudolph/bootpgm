bcdc9d1f69a50b26a365dfff51a92b7481c37498
Johannes <Johannes@cfde777c-580b-0410-a363-983e57c4def2>
2006-01-21 00:09:50 +0000

    - implemented readln
    git-svn-id: svn://virtual-void.net/development/native/trunk@85 cfde777c-580b-0410-a363-983e57c4def2

--- usermode/IO.cpp
+++ usermode/IO.cpp
@@ -13,3 +13,21 @@ void IO::println(char *buffer)
 	print(buffer);
 	print("\n");
 }
+
+void IO::readln(char *buffer,unsigned int length)
+{
+	//NT::DbgPrint("readln startet\n");
+	unsigned int curlength=0;
+	char curChar[2];
+	curChar[0]=0;
+	curChar[1]=0;
+	while (curlength<length&&curChar[0]!='\n')
+	{
+		curChar[0]=getChar();
+		buffer[curlength]=curChar[0];
+		print(curChar);
+		curlength++;
+	}
+	buffer[curlength-1]=0;
+	//NT::DbgPrint("readln beendet\n");
+}
--- usermode/IO.h
+++ usermode/IO.h
@@ -7,6 +7,8 @@ public:
 	virtual char getChar()=0;
 	virtual void print(char *buffer)=0;
 	void println(char *buffer);
+	virtual void*malloc(unsigned int length)=0;
 	~IO(void);
 	virtual char *getVersion()=0;
+	void readln(char *buffer,unsigned int length);
 };
