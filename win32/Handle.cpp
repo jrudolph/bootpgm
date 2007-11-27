befb69ec99f6cd4d714292747b2815a9f924d328
johannes <johannes@cfde777c-580b-0410-a363-983e57c4def2>
2007-11-27 19:14:37 +0000

    * cleaned up wrapper classes, several new Registry funcs
    git-svn-id: svn://virtual-void.net/development/native/trunk@311 cfde777c-580b-0410-a363-983e57c4def2

--- win32/Handle.cpp
+++ win32/Handle.cpp
@@ -2,28 +2,28 @@
 #include "Handle.h"
 #include "main.h"
 
-#define CHECKER(status) ::handle(status, __FILE__ ,TOSTRING(__LINE__));
+void debug(char *msg)
+{
+	mainSingleton->get_io().debugout(msg);
+}
 
 void handle(ULONG status,char *file,char *line)
 {
 	if (status!=STATUS_SUCCESS)
 	{
-		char *buffer=new char[5000];
-		sprintf_s(buffer,5000,"Fehler in %s Zeile %s: 0x%X",file,line,status);
-		throw buffer;
+		char *buffer = new char[5000];
+		sprintf(buffer,"Fehler in %s Zeile %s: 0x%X",file,line,status);
+		mainSingleton->get_io().println(buffer);
+		mainSingleton->get_io().readln(buffer,sizeof(buffer));
 	}
-}
-
-void debug(char *msg)
-{
-	mainSingleton->get_io().debugout(msg);
 }
 
 WinObject::~WinObject()
 {
 	char buffer[500];
 	debug((L"Closing Handle to '" + get_name() + L"'" ).chars(buffer,500));
-	ZwClose(handle);
+	ULONG status = ZwClose(handle);
+	CHECKER(status)
 }
 
 UnicodeString WinObject::get_name()
@@ -62,9 +62,9 @@ UnicodeString WinObject::get_name()
 
 UnicodeString::UnicodeString(const wchar_t *chars)
 {
-	size_t len=wcslen(chars);
+	unsigned short len = (unsigned short)wcslen(chars);
 	wchar_t *buffer=new wchar_t[len];
-	string.Length=len*sizeof(wchar_t);
+	string.Length= len * sizeof(wchar_t);
 	string.Buffer=buffer;
 	string.MaximumLength=string.Length;
 	memcpy(buffer,chars,len*2);
@@ -73,9 +73,29 @@ UnicodeString::UnicodeString(const wchar_t *chars)
 	char buffer2[500];
 	debug(this->chars(buffer2,500));*/
 }
-UnicodeString & UnicodeString::operator=(wchar_t *chars)
+UnicodeString::UnicodeString(const wchar_t *chars,unsigned int length)
 {
-	size_t len= wcslen(chars) * 2;
+	string.Length = (USHORT)length;
+	string.MaximumLength = (USHORT)length;
+	string.Buffer = new wchar_t[length/2];
+	memcpy(string.Buffer,chars,length);
+}
+UnicodeString::UnicodeString(const char *chars)
+{
+	unsigned short l=(unsigned short)strlen(chars);
+	wchar_t *wcs=new wchar_t[l];
+	mbstowcs(wcs,chars,l);
+	string.Length = l*2;
+	string.MaximumLength = l*2;
+	string.Buffer = wcs;
+}
+/*UnicodeString &UnicodeString::operator=(wchar_t *chars)
+{
+	char buffer[1000];
+	_snprintf(buffer,1000,"UnicodeString = called old = %S, new = %S",string.Buffer,chars);
+	debug(buffer);
+
+	unsigned short len= (unsigned short)wcslen(chars) * 2;
 
 	if (len > string.MaximumLength)
 	{
@@ -88,12 +108,12 @@ UnicodeString & UnicodeString::operator=(wchar_t *chars)
 	string.Length = len;
 
 	return *this;
-}
+}*/
 UnicodeString::~UnicodeString()
 {
-	//debug("UnicodeString deleted:");
-	//char buffer[500];
-	//debug(this->chars(buffer,500));
+	debug("UnicodeString deleted:");
+	char buffer[500];
+	debug(this->chars(buffer,500));
 
 	delete string.Buffer;
 }
@@ -111,7 +131,7 @@ UnicodeString UnicodeString::operator+(UnicodeString&str2)
 	return UnicodeString(str);
 }
 
-int RegKey::get_value(UnicodeString &path,PULONG type,void *buffer,int length)
+int RegKey::get_value(UnicodeString *path,PULONG type,void *buffer,int length)
 {
 	int len=length + sizeof(KEY_VALUE_PARTIAL_INFORMATION);
 	void *temp=_alloca(len);
@@ -119,7 +139,7 @@ int RegKey::get_value(UnicodeString &path,PULONG type,void *buffer,int length)
 	KEY_VALUE_PARTIAL_INFORMATION *pinfo = reinterpret_cast<KEY_VALUE_PARTIAL_INFORMATION*>(temp);
 	ULONG status=ZwQueryValueKey(
 		handle
-		,&path.unicode_string()
+		,&path->unicode_string()
 		,KeyValuePartialInformation
 		,pinfo
 		,len
@@ -127,60 +147,146 @@ int RegKey::get_value(UnicodeString &path,PULONG type,void *buffer,int length)
 
 	CHECKER(status)
 
+	if (status!=STATUS_SUCCESS)
+		return 0;
+
 	memcpy(buffer,pinfo->Data,pinfo->DataLength);
 	*type = pinfo->Type;
 	return pinfo->DataLength;
 }
 
-void RegKey::get_string_value(UnicodeString &name,UnicodeString&res)
+void RegKey::set_value(UnicodeString *path,ULONG type,void *data,ULONG size)
+{
+	ULONG status = ZwSetValueKey(
+		handle
+		,&path->unicode_string()
+		,0
+		,type
+		,data
+		,size);
+	CHECKER(status)
+}
+
+UnicodeString RegKey::get_string_value(UnicodeString *name)
 {
 	char buffer[500];
 	ULONG type;
 	ULONG len=get_value(name,&type,buffer,sizeof(buffer));
 	
-	if (type!=REG_SZ)
-		throw "Only REG_SZ allowed in get_string_value";
+	if (type!=REG_SZ){
+		debug("Only REG_SZ allowed in get_string_value");
+		return (wchar_t*)0;
+	}
 	
 	wchar_t *value= reinterpret_cast<wchar_t*>(buffer);
 	value[min(sizeof(buffer)-1,len)] = 0;
-	res = value;
+	return value;
 }
 
-HANDLE RegKey::open_key(UnicodeString &path)
+HANDLE RegKey::open_key(HANDLE parent,UnicodeString &path)
 {
 	OBJECT_ATTRIBUTES ObjectAttributes;
 	
-	UnicodeString realPath = UnicodeString(L"\\Registry\\") + path;
+	UnicodeString fullPath = UnicodeString(L"\\Registry\\") + path;
+	UnicodeString *pp = parent ? &path : &fullPath;
 
 	InitializeObjectAttributes(
 		&ObjectAttributes,
-		const_cast<UNICODE_STRING*>(&realPath.unicode_string()),
+		const_cast<UNICODE_STRING*>(&pp->unicode_string()),
 		OBJ_CASE_INSENSITIVE,
-		NULL,
+		parent,
 		NULL);
 
 	HANDLE h;
 	ULONG Disposition;
-	ULONG status = ZwCreateKey(
+	ULONG status = ZwOpenKey(
 		&h,
 		KEY_ALL_ACCESS,
-		&ObjectAttributes,
-		0,
-		NULL,
-		REG_OPTION_NON_VOLATILE,
-		&Disposition);
+		&ObjectAttributes);
+
+	if (status == 0xC000034){
+		IO &io=mainSingleton->get_io();
+		io.print("Not found: ");
+		char buffer[1000];
+		io.println(path.chars(buffer,sizeof(buffer)));
+	}
 
 	CHECKER(status);
 
 	return status == STATUS_SUCCESS? h : 0;
 }
 
-void testRegKey(IO &io,char *args)
+void RegKey::flush()
 {
-	RegKey k(L"Machine\\SYSTEM\\CurrentContro4lSet\\Control\\ComputerName\\ComputerName");
-	UnicodeString res(L"");
-	k.get_string_value(UnicodeString(L"ComputerName"),res);
+	ULONG status=NtFlushKey(handle);
+	CHECKER(status);
+}
 
-	char buffer[500];
-	io.println(res.chars(buffer,500));
+void RegKey::save_to(HANDLE fileHandle)
+{
+	ULONG status = NtSaveKey(handle,fileHandle);
+	CHECKER(status);
+}
+
+void RegKey::print_subkeys(IO &io)
+{
+	ULONG status = STATUS_SUCCESS;
+	int i=0;
+	char buffer[1000];
+	do
+	{		
+		ULONG res;
+		KEY_BASIC_INFORMATION *info = reinterpret_cast<KEY_BASIC_INFORMATION*>(buffer);
+		status = ZwEnumerateKey(
+			handle
+			,i
+			,KeyBasicInformation
+			,buffer
+			,sizeof(buffer)
+			,&res);
+
+		if (status == STATUS_SUCCESS)
+		{
+			UnicodeString name(info->Name,info->NameLength);
+			io.println(name.chars(buffer,sizeof(buffer)));
+		}
+
+		i++;
+	} while (status == STATUS_SUCCESS);
+}
+void RegKey::print_values(IO &io)
+{
+	ULONG status = STATUS_SUCCESS;
+	int i=0;
+	char buffer[1000];
+	do
+	{
+		ULONG len;
+		status = ZwEnumerateValueKey(
+			handle
+			,i
+			,KeyValueBasicInformation
+			,buffer
+			,sizeof(buffer)-2
+			,&len);
+		
+		if (status==STATUS_SUCCESS)
+		{
+			KEY_VALUE_BASIC_INFORMATION *info = reinterpret_cast<KEY_VALUE_BASIC_INFORMATION*>(buffer);
+			info->Name[info->NameLength/2] = 0;
+			char buf[100];
+			_snprintf(buf,sizeof(buf),"%-30S | %-15s",info->Name,type_to_name(info->Type));
+			io.println(buf);			
+		}
+
+		i++;
+	} while(status == STATUS_SUCCESS);
+}
+RegKey *RegKey::subkey(UnicodeString&name)
+{
+	HANDLE h = open_key(handle,name);
+	if (h)
+		return new RegKey(h);
+	else
+		return 0;
 }
--- win32/Handle.h
+++ win32/Handle.h
@@ -1,11 +1,15 @@
 #pragma once
 
+class IO;
+
 class UnicodeString
 {
 	NT::UNICODE_STRING string;
 	UnicodeString(UNICODE_STRING str):string(str){}
 public:
 	UnicodeString(const wchar_t *chars);
+	UnicodeString(const wchar_t *chars,unsigned int length);
+	UnicodeString(const char *chars);
 	UNICODE_STRING& unicode_string(){return string;}
 	virtual ~UnicodeString();
 	static UnicodeString& from_unicode(UNICODE_STRING str)
@@ -15,11 +19,12 @@ public:
 		memcpy(str2.Buffer,str.Buffer,str.Length);
 		return *new UnicodeString(str2);
 	}
-	UnicodeString& operator=(wchar_t *new_data);
+	//UnicodeString& operator=(wchar_t *new_data);
+	//UnicodeString& operator=(UnicodeString &other);
 	char *chars(char *buffer,size_t len)
 	{
-		wcstombs(buffer,string.Buffer,min(len,string.Length/2));
-		buffer[min(len-1,string.Length/2)]=0;
+		wcstombs(buffer,string.Buffer,min(len,(unsigned int)string.Length/2));
+		buffer[min(len-1,(unsigned int)string.Length/2)]=0;
 		return buffer;
 	}
 	UnicodeString operator+(UnicodeString&str2);
@@ -43,15 +48,43 @@ public:
 public:
 	~WinObject();
 	UnicodeString get_name();
+	HANDLE get_handle(){return handle;}
+	bool valid(){return handle != 0;}
 };
 
 class RegKey:public WinObject
 {
 private:
-	static HANDLE open_key(UnicodeString &path);
+	static HANDLE open_key(HANDLE parent,UnicodeString &path);
+	RegKey(HANDLE h):WinObject(h){}
 public:
-	RegKey(UnicodeString &path):WinObject(open_key(path)){}
-	RegKey(const wchar_t *path):WinObject(open_key(UnicodeString(path))){}
-	int get_value(UnicodeString &path,PULONG type,void *buffer,int length);
-	void get_string_value(UnicodeString &name,UnicodeString&res);
+	RegKey(UnicodeString &path):WinObject(open_key(0,path)){}
+	RegKey(const wchar_t *path):WinObject(open_key(0,UnicodeString(path))){}
+
+	int get_value(UnicodeString *path,PULONG type,void *buffer,int length);
+	UnicodeString get_string_value(UnicodeString *name);
+	void set_value(UnicodeString *path,ULONG type,void *data,ULONG size);
+	void flush();
+	void save_to(HANDLE fileHandle);
+
+	RegKey *subkey(UnicodeString&name);
+
+	void print_subkeys(IO &io);
+	void print_values(IO &io);
+	static const char *type_to_name(ULONG type)
+	{
+		switch(type)
+		{
+		case REG_SZ:
+			return "String";
+		case REG_EXPAND_SZ:
+			return "Expandable String";
+		case REG_MULTI_SZ:
+			return "Multiline String";
+		case REG_DWORD:
+			return "DWORD";
+		default:
+			return "Unknown";
+		}
+	}
 };
